5169591485b5e1f13c54811f73cc8dbec5b7c281
Wágner Magno <wagner.sales@digitalbusiness.com.br>
2022-03-03 08:06:07 -0300

    Criando rota de bed e partograma

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use('/', require('./route/index'));
+// app.use('/', require('./route/index'));
+app.use('/', require('./route/userRoute'));
+
 app.use(function (error, req, res, next) {
 	if (error.message === 'Email ou senha não estão corretos!') {
 		return res.status(404).send(error.message);
