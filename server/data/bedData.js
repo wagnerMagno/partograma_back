195823d5eaa5f761d77c832c8e5880706e8560b2
Wágner Magno <wagner.sales@digitalbusiness.com.br>
2022-05-11 14:17:53 -0300

    Criando qtdContraction

--- server/data/bedData.js
+++ server/data/bedData.js
@@ -2,9 +2,9 @@ const database = require('../infra/database');
 
 
 exports.createBed = function (bed) {
+
 	return database.oneOrNone(`INSERT INTO public.bed( id_user, name_pacient, number, age, dum, medication, comorbidity, gesta, para, aborto, usg)
-	   VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
-		, [bed.id_user, bed.name_pacient, bed.number, bed.age, bed.dum, bed.medication, bed.comorbidity, bed.gesta, bed.para, bed.aborto, bed.usg]);
+	   VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)` , [bed.id_user, bed.name_pacient, bed.number, bed.age, bed.dum, bed.medication, bed.comorbidity, bed.gesta, bed.para, bed.aborto, bed.usg]);
 };
 
 exports.getById = function (id) {
@@ -31,9 +31,9 @@ exports.close = function (bed) {
 
 exports.insertPartograma = function (obj) {
 	return database.oneOrNone(`INSERT INTO public.partogram(
-		id_bed, dilation, contraction, fcf, start_tay, real_time, registration_time, handbag, la, oxytocin)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
-		[obj.id_bed, obj.dilation, obj.contraction, obj.fcf, obj.start_tay, obj.real_time, obj.registration_time, obj.handbag, obj.la, obj.oxytocin])
+		id_bed, dilation, contraction, fcf, start_tay, real_time, registration_time, handbag, la, oxytocin, qtd_contraction)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
+		[obj.id_bed, obj.dilation, obj.contraction, obj.fcf, obj.start_tay, obj.real_time, obj.registration_time, obj.handbag, obj.la, obj.oxytocin, obj.qtdContraction])
 }
 
 exports.getPartogramaByIdBed = function (id) {
